f622a08eb18d572335ab7db998030add923bad3a
Mahendra Dwi Purwanto <mahendradwipurwanto@gmail.com>
2022-08-01 20:41:24 +0700

    add landing page

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,5 +2,5 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 $route['default_controller'] = 'home';
-$route['404_override'] = 'utility/not_found';
+$route['404_override'] = '';
 $route['translate_uri_dashes'] = false;
---
+++ application/views/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<title>SwitchGame</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Switchgame !</h1>
+    <div>
+        <p>A simple game with a simple perpose!</p>
+        <p>&mdash; Switchgame admin</p>
+    </div>
+</article>
